3b77ad8980e6a7ce9a21acd41ed79c44b0423184
Hoomi1 <glebbambam@yandex.ru>
2022-09-07 14:12:20 +0300

    README create, project work

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # fix-me
+***
+
+>Develop multi-threaded network applications with asynchronous sockets and the java executor
+framework.
+
+### ___Utils:___
+    Framework Netty
+    Asynchronous sockets
+    Non-bloking sockets
+    Chain of responsibility design pattern
+### ___Run:___
+* *Build project*
+```
+mvn clean project
+```
+* *Build router* 
+```
+java -jar router/target/router.jar
+```
+* *Build market*
+```
+java -jar market/target/market.jar
+```
+* *Build broker*
+```
+java -jar broker/target/broker.jar
+```
--- utils/src/main/java/ft/school21/fix_utils/Database/Transaction.java
+++ utils/src/main/java/ft/school21/fix_utils/Database/Transaction.java
@@ -1,36 +1,36 @@
-package ft.school21.fix_utils.Database;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-
-@Entity
-@Table(name = "transactions")
-public class Transaction {
-
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "id")
-//    private int id;
-
-    @Column(name = "tran")
-    private String tran;
-
-    public Transaction() {
-    }
-
-    public Transaction(String tran) {
-        this.tran = tran;
-    }
-
-    public String getTran() {
-        return tran;
-    }
-
-    public void setTran(String tran) {
-        this.tran = tran;
-    }
-}
+//package ft.school21.fix_utils.Database;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//
+//@Entity
+//@Table(name = "transactions")
+//public class Transaction {
+//
+////    @Id
+////    @GeneratedValue(strategy = GenerationType.IDENTITY)
+////    @Column(name = "id")
+////    private int id;
+//
+//    @Column(name = "tran")
+//    private String tran;
+//
+//    public Transaction() {
+//    }
+//
+//    public Transaction(String tran) {
+//        this.tran = tran;
+//    }
+//
+//    public String getTran() {
+//        return tran;
+//    }
+//
+//    public void setTran(String tran) {
+//        this.tran = tran;
+//    }
+//}
